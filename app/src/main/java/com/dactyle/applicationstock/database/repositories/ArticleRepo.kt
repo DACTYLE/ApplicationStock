a326dd3cb43d4d1112a54450543bf09dbd717127
sgo <s.gosset@axesys.fr>
2024-11-04 17:04:49 +0100

    Remove redundant suspend

--- app/src/main/java/com/dactyle/applicationstock/database/repositories/ArticleRepo.kt
+++ app/src/main/java/com/dactyle/applicationstock/database/repositories/ArticleRepo.kt
@@ -1,6 +1,5 @@
 package com.dactyle.applicationstock.database.repositories
 
-import androidx.lifecycle.LiveData
 import com.dactyle.applicationstock.database.entities.Article
 import com.dactyle.applicationstock.database.entities.ArticleDAO
 import kotlinx.coroutines.flow.Flow
@@ -8,17 +7,17 @@ import kotlinx.coroutines.flow.Flow
 class ArticleRepo (private val articleDao: ArticleDAO) {
 
     // Méthode pour obtenir tous les articles
-    suspend fun getAllArticles(): Flow<List<Article>> {
+    fun getAllArticles(): Flow<List<Article>> {
         return articleDao.getAllArticles()
     }
 
     // Méthode pour insérer un article
-    suspend fun insert(article: Article) {
+    fun insert(article: Article) {
         articleDao.insert(article)
     }
 
     // Méthode pour obtenir les articles par catégorie
-    suspend fun getArticlesByCategory(category: String): Flow<List<Article>> {
+    fun getArticlesByCategory(category: String): Flow<List<Article>> {
         return articleDao.getArticlesByCategory(category)
     }
 }
